611f30e335006c07ee64ab2afaca6157be7715bd
aymen <92206771+aymen065@users.noreply.github.com>
2023-12-25 00:46:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,6 +26,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   network_profile {
     load_balancer_sku = "Standard"
-    network_plugin    = "kubenet" # azure (CNI)
+    network_plugin    = "kubenet"
   }
 }
